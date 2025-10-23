07abd8f1a7309a009653fb5550393fc22f878cd5
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-10-23 12:57:39 +0200

    1

---
+++ 23.10/curiculum.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template_string
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def cv():
+    with open("curiculum.json",encoding="utf-8") as f:
+        data = json.load(f)
+    dp = data["datos_personales"]
+
+    with open("cuirculum.html",encoding="utf-8") as f:
+        html = f.read()
+    
+    return render_template_string(html,**dp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
