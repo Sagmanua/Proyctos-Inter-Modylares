0b0ce272f8c6110b67f9fd6bafbbb952cc7de714
Sagmanua <163444640+Sagmanua@users.noreply.github.com>
2025-11-06 12:15:28 +0100

    1

--- practica/Eval1/Tema1/003/curiculum.py
+++ practica/Eval1/Tema1/003/curiculum.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def cv():
-    with open("curiculum.json",encoding="utf-8") as f:
+    with open("curriculum.json", encoding="utf-8") as f:
         data = json.load(f)
     dp = data["datos_personales"]
 
-    with open("index.html",encoding="utf-8") as f:
+    with open("curriculum.html", encoding="utf-8") as f:
         html = f.read()
-    
-    return render_template_string(html,**dp)
+
+    return render_template_string(html, **dp)
 
 if __name__ == "__main__":
     app.run(debug=True)
