ec984cf36ae1744ef013401bedfa0a98499ea10c
Bohdan Sydorenko <163444640+Sagmanua@users.noreply.github.com>
2025-12-18 12:49:21 +0100

    1

--- Explication/102 Ejercicios practicas segundo trimestre /201-Proyectos Alumnos/003 Andres/index2.php
+++ Explication/102 Ejercicios practicas segundo trimestre /201-Proyectos Alumnos/003 Andres/index2.php
@@ -3,10 +3,23 @@
 	<head>
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-    	#ruleta{transform:rotate(45deg);}
+    	#ruleta{animation:frena 10s;width:500px;height:500px;position:relative;}
+      @keyframes frena{
+      	0%{transform:rotate(0deg);}
+        100%{transform:rotate(359deg);}
+      }
+      #bolita{position:absolute;top:150px;left:150px;background:white;width:20px;
+      height:20px;border-radius:20px;animation:mueve_bola 10s;}
+      @keyframes mueve_bola{
+      	0%{left:250px;top:250px;}
+        100%{left:250px;top:400px;}
+      }
     </style>
   </head>
   <body>
-  	<img id="ruleta" src="ruleta.jpg">
+  	<div id="ruleta">
+  		<img  src="ruleta.jpg">
+      <div id="bolita"></div>
+    </div>
   </body>
 </html>
